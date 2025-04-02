ecb5ec4e4f0a1cfe14667d4b60dd1e83585bbb76
Evgenia <zenataraskina226@gmail.com>
2025-04-02 17:37:06 +0300

    wrote 4 function

--- Taraskina_LR3_project.cpp
+++ Taraskina_LR3_project.cpp
@@ -1,67 +1,77 @@
 #include <iostream>
+#include <cmath> // Для функции округления
 using namespace std;
 
-
-
-double numberX(double x ){
-    //разрабатывается Ивановым П. - ветка branch_fun_1
-    cin>>x;
-}
-double numberY( double y){
-    //разрабатывается Ивановым П. - ветка branch_fun_1
-    cin>>y;
-}
-double numberZ(double z){
-    //разрабатывается Ивановым П. - ветка branch_fun_1
-    cin>>z;
-}
-double DrobPart(double x,double y, double z)
-{
-    //разрабатывается Петровым П. - ветка branch_fun_2
+double numberX() {
+    double x;
+    cin >> x;
+    return x;
 }
 
-double Okruglenie() 
-{
-    //разрабатывается Василенко А. - ветка branch_fun_3
+double numberY() {
+    double y;
+    cin >> y;
+    return y;
 }
 
+double numberZ() {
+    double z;
+    cin >> z;
+    return z;
+}
+
+double DrobPart(double summ) {
+    return summ - static_cast<int>(summ);
+}
+
+int Okruglenie(double summ) {
+    
+}
 
 int main() {
-    double x, y, z;
-    double menu;
-    
+    setlocale(LC_ALL, "rus");
+    double x , y , z ; // Инициализация переменных
+    int menu;
 
     do {
         cout << "Task 1. Ввести вещественное число x" << endl;
         cout << "Task 2. Ввести вещественное число y" << endl;
         cout << "Task 3. Ввести вещественное число z" << endl;
-        cout << "Task 4. Найти дробную часть суммы x,y,z" << endl;
+        cout << "Task 4. Найти дробную часть суммы x, y, z" << endl;
         cout << "Task 5. Округлить до ближайшего целого сумму этих 3-х чисел" << endl;
+        cout << "Введите номер задачи (0 для выхода): ";
         cin >> menu;
 
         switch (menu) {
-            case 1:
-                cout << "Введите число x " << num(x) << endl;
-                break;
-            case 2:
-                cout << "Введите число y " << num(y) << endl;
-                break;
-            case 3:
-                cout << "Введите число z " << num(z) << endl;
-                break;
-            case 4:
-                cout << "Найти дробную часть суммы этих 3-х чисел " << DrobPart(x,y,z) << endl;
-                break;
-            case 5:
-                cout << "Введите число х " << num(x) << endl;
-                break;
-            case 0:
-                cout << "Выход из приложения." << endl;
-                break;
-            default:
-                cout << "Некорректный выбор. Пожалуйста, попробуйте снова." << endl;
+        case 1:
+            cout << "Введите число x: ";
+            x = numberX();
+            break;
+        case 2:
+            cout << "Введите число y: ";
+            y = numberY();
+            break;
+        case 3:
+            cout << "Введите число z: ";
+            z = numberZ();
+            break;
+        case 4: {
+            double summ = x + y + z; // Вычисляем сумму здесь
+            cout << "Дробная часть суммы: " << DrobPart(summ) << endl;
+            break;
         }
-    } while (choice != 0);
+        case 5: {
+            double summ = x + y + z; // Вычисляем сумму здесь
+            cout << "Округленная сумма: " << Okruglenie(summ) << endl;
+            break;
+        }
+        case 0:
+            cout << "Выход из приложения." << endl;
+            break;
+        default:
+            cout << "Некорректный выбор. Пожалуйста, попробуйте снова." << endl;
+        }
+    } while (menu != 0);
 
     return 0;
 }
